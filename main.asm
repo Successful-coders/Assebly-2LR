854d1c39bdb81f5438ea4a7512d9050648879b15
Voronik <divi1801@icloud.com>
2020-10-28 19:11:12 +0700

    Work 2d cycle, right find symbol, need add exiting parametrs

--- main.asm
+++ main.asm
@@ -1,21 +1,19 @@
 .386
 .model flat
-.stack 4096
+;.stack 4096
 
-;extern  lstrlenA@4: PROC	; функция определения длины строки
-;EXTERN  CharToOemA@8: PROC
 .data
 flenght	dd 0
 slenght	dd 0
+help dd 0
 string1 dd 0
 string2 dd 0
+pos dd 0
 .code
 
 @FUNC@20 proc
 	push ebp ;сохраняем ebp при вызове
 	mov ebp, esp ;устанавливаем новое значение ebp
-
-	
 
 	;длина второй строки должна быть больше длины первой строки
 	cmp edx, ecx
@@ -30,17 +28,43 @@ string2 dd 0
 	add slenght, edx
 	cmp slenght, 0
 	jbe error
+	add help, edx
 
+	;add string1, [ebp]+4 строка приемник, откуда берем символы
+ 
+	;add string2, [ebp]+8 сттрока в которой ищем
+
+	;[ebp+16] - pos 
+	
+	mov esi, [ebp]+12;вторая строка
+	mov edi, [ebp]+8;первая строка
 	xor eax, eax
-	mov eax, [ebp]+4
-	add string1, eax
-
-	xor ecx, ecx
-	mov ecx, [ebp]+8
-	add string2, ecx
-
-	xor edx, edx
-	mov edx, [ebp+16]
+	xor esp, esp
+FindSymbol:
+;работает
+	mov al,[edi]
+	mov cl, [esi]
+	cmp al, cl
+	je found
+	inc esi
+	dec slenght
+	cmp slenght, 0
+	ja FindSymbol
+	
+	inc edi
+	mov esi, [ebp]+12
+	add slenght, edx
+	add esp ,1
+	dec flenght
+	cmp flenght, 0
+	ja FindSymbol
+;добавить выход когда ничего не нашли
+	pop ebp
+	mov eax,1
+	ret 20
+;добавить когда нашли
+found:
+	ret 1
 
 	error:
 		pop ebp
