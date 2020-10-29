e114db7ed37304d223114e3ffe64718755675089
Voronik <divi1801@icloud.com>
2020-10-29 23:28:29 +0700

    delete all local variables, need to edit first string

--- Source.cpp
+++ Source.cpp
@@ -7,68 +7,28 @@
 
 extern "C" int __fastcall FUNC(int fLenght, int sLenght, char *firststring,  char *secondstring, int pos);
 
-void Error()
-{
 
-}
-int FUNCx(char* firststring, int fLengh, char* secondstring, int sLengh, int pos)
-{
-
-	firststring;
-	if (fLengh == 0)
-	{
-		return -1;
-	}
-	if (sLengh == 0)
-		return -1;
-	if (sLengh < fLengh)
-		return -1;
-	int k = 0;
-	for (int i = 0; i <= fLengh; i++)
-	{
-		k = 0;
-		for (int j = 0; j <= sLengh; j++)
-		{
-			if (firststring[i] == secondstring[j])
-			{
-				k++;
-			}
-
-		}
-		if (k == 0)
-		{
-			pos = i + 1;
-			return pos;
-		}
-	}
-	return pos;
-}
  
 void main()
 {
 	char* string1, * string2;
 	string1 = new char[200];
 	string2 = new char[200];
-	int pos = 0;
+	int pos = 1;
 	printf("Enter first string:\n");
 	scanf("%s", string1);
 	printf("Enter second string:\n");
 	scanf("%s", string2);
 
-	pos = FUNC(strlen(string1),strlen(string2),string1,  string2,  pos);
-	if (pos == -1)
+	
+	if (FUNC(strlen(string1), strlen(string2), string1, string2, pos) == -1)
 	{
 		printf("Incorrect input data.");
 	}
 	else
 	{
-		if (pos != 0)
-		{
-			printf_s("Result: %i", pos);
-		}
-		else
-		{
-			printf_s("All letter in first string not including in second string");
-		}
+		
+			printf_s("Result: %i", string1);
+		
 	}
 }
